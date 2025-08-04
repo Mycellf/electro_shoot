725f3967ee828f0f4f105ab54ca0901bc06c54d1
jeffrey <jcouch210@gmail.com>
2025-08-04 00:46:24 -0800

    Add enemy textures

--- src/enemy.rs
+++ src/enemy.rs
@@ -1,63 +1,79 @@
 use std::{
     f64::consts::TAU,
     ops::{Deref, DerefMut},
+    sync::LazyLock,
 };
 
+use macroquad::{
+    color::colors,
+    texture::{self, DrawTextureParams},
+};
 use nalgebra::{Isometry2, UnitComplex, vector};
 
-use crate::{object::Object, shape::Shape};
+use crate::{
+    object::Object,
+    shape::Shape,
+    utils::{self, TEXTURES, TextureEntry},
+};
 
-pub static ENEMY_KINDS: [EnemyKind; 5] = [
-    EnemyKind {
-        name: "Red Circle",
-        properties: EnemyProperties {
-            shape: Shape::Circle { radius: 0.5 },
-            speed: 3.0,
-            angular_velocity: 0.0,
-            maximum_health: 4,
-        },
-    },
-    EnemyKind {
-        name: "Purple Circle",
-        properties: EnemyProperties {
-            shape: Shape::Circle { radius: 0.5 },
-            speed: 9.0,
-            angular_velocity: 0.0,
-            maximum_health: 4,
-        },
-    },
-    EnemyKind {
-        name: "Electric Circle",
-        properties: EnemyProperties {
-            shape: Shape::Circle { radius: 0.6 },
-            speed: 12.0,
-            angular_velocity: 0.0,
-            maximum_health: 4,
-        },
-    },
-    EnemyKind {
-        name: "Red Square",
-        properties: EnemyProperties {
-            shape: Shape::Rectangle {
-                half_size: vector![0.6, 0.6],
+pub static ENEMY_KINDS: LazyLock<[EnemyKind; 5]> = LazyLock::new(|| {
+    [
+        EnemyKind {
+            name: "Red Circle",
+            properties: EnemyProperties {
+                shape: Shape::Circle { radius: 0.5 },
+                speed: 3.0,
+                angular_velocity: 0.0,
+                maximum_health: 4,
+                texture: &TEXTURES[0],
             },
-            speed: 3.0,
-            angular_velocity: -5.0 / 24.0 * TAU,
-            maximum_health: 8,
         },
-    },
-    EnemyKind {
-        name: "Purple Square",
-        properties: EnemyProperties {
-            shape: Shape::Rectangle {
-                half_size: vector![0.8, 0.8],
+        EnemyKind {
+            name: "Purple Circle",
+            properties: EnemyProperties {
+                shape: Shape::Circle { radius: 0.5 },
+                speed: 9.0,
+                angular_velocity: 0.0,
+                maximum_health: 4,
+                texture: &TEXTURES[1],
             },
-            speed: 3.0,
-            angular_velocity: 1.0 / 6.0 * TAU,
-            maximum_health: 12,
         },
-    },
-];
+        EnemyKind {
+            name: "Electric Circle",
+            properties: EnemyProperties {
+                shape: Shape::Circle { radius: 0.6 },
+                speed: 12.0,
+                angular_velocity: 0.0,
+                maximum_health: 4,
+                texture: &TEXTURES[2],
+            },
+        },
+        EnemyKind {
+            name: "Red Square",
+            properties: EnemyProperties {
+                shape: Shape::Rectangle {
+                    half_size: vector![0.6, 0.6],
+                },
+                speed: 3.0,
+                angular_velocity: -5.0 / 24.0 * TAU,
+                maximum_health: 8,
+                texture: &TEXTURES[3],
+            },
+        },
+        EnemyKind {
+            name: "Purple Square",
+            properties: EnemyProperties {
+                shape: Shape::Rectangle {
+                    half_size: vector![0.8, 0.8],
+                },
+                speed: 3.0,
+                angular_velocity: 1.0 / 6.0 * TAU,
+                maximum_health: 12,
+                texture: &TEXTURES[4],
+            },
+        },
+    ]
+});
 
 #[derive(Clone, Debug)]
 pub struct Enemy {
@@ -68,6 +84,9 @@ pub struct Enemy {
 
     pub health: u32,
     pub time_since_hit: f64,
+
+    pub brightness: f64,
+    pub brightness_update_time: f64,
 }
 
 #[derive(Clone, Debug)]
@@ -84,6 +103,7 @@ pub struct EnemyProperties {
     pub angular_velocity: f64,
 
     pub maximum_health: u32,
+    pub texture: &'static TextureEntry,
 }
 
 impl Enemy {
@@ -101,16 +121,48 @@ impl Enemy {
             properties: kind.properties,
             health: kind.properties.maximum_health,
             time_since_hit: f64::INFINITY,
+            brightness: 0.0,
+            brightness_update_time: 0.0,
         }
     }
 
     pub fn tick(&mut self, dt: f64) {
-        let speed = self.properties.speed * (self.time_since_hit / Self::SLOWDOWN_TIME).min(1.0);
+        let speed = self.properties.speed * self.speed_multiplier();
         self.object.linear_velocity = self.direction * vector![speed, 0.0];
 
         self.object.tick(dt);
 
+        self.brightness_update_time += dt * 30.0;
+
+        if self.brightness_update_time > 1.0 {
+            self.brightness_update_time %= 1.0;
+            self.brightness = macroquad::rand::gen_range(self.speed_multiplier(), 1.0);
+        }
+
         self.time_since_hit += dt;
+    }
+
+    pub fn draw(&self) {
+        let size = self.properties.texture.size() * 0.1;
+
+        texture::draw_texture_ex(
+            &self.properties.texture,
+            self.position.translation.x as f32 - size.x / 2.0,
+            self.position.translation.y as f32 - size.y / 2.0,
+            utils::darken_color(colors::WHITE, self.brightness),
+            DrawTextureParams {
+                dest_size: Some(size),
+                source: None,
+                rotation: self.position.rotation.angle() as f32,
+                flip_x: false,
+                flip_y: false,
+                pivot: None,
+            },
+        );
+    }
+
+    pub fn speed_multiplier(&self) -> f64 {
+        (self.time_since_hit / Self::SLOWDOWN_TIME).min(1.0)
     }
 
     pub fn hit(&mut self, damage: u32) {
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,14 @@
-use macroquad::{camera::Camera2D, input, math::Vec2, window};
+use std::{ops::Deref, sync::LazyLock};
+
+use macroquad::{
+    Error,
+    camera::Camera2D,
+    color::Color,
+    input,
+    math::Vec2,
+    texture::{FilterMode, Image, Texture2D},
+    window,
+};
 use nalgebra::Vector2;
 
 #[must_use]
@@ -14,3 +24,46 @@ pub fn mouse_position(camera: &Camera2D) -> Vector2<f64> {
 pub fn update_camera_aspect_ratio(camera: &mut Camera2D) {
     camera.zoom.x = camera.zoom.y.abs() * window::screen_height() / window::screen_width();
 }
+
+pub fn darken_color(color: Color, brightness: f64) -> Color {
+    Color {
+        r: color.r * brightness as f32,
+        g: color.g * brightness as f32,
+        b: color.b * brightness as f32,
+        a: color.a,
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct TextureEntry {
+    pub image: Image,
+    pub texture: Texture2D,
+}
+
+impl TextureEntry {
+    pub fn from_bytes(bytes: &[u8]) -> Result<Self, Error> {
+        let image = Image::from_file_with_format(bytes, None)?;
+        let texture = Texture2D::from_image(&image);
+        texture.set_filter(FilterMode::Nearest);
+
+        Ok(Self { image, texture })
+    }
+}
+
+impl Deref for TextureEntry {
+    type Target = Texture2D;
+
+    fn deref(&self) -> &Self::Target {
+        &self.texture
+    }
+}
+
+pub static TEXTURES: LazyLock<[TextureEntry; 5]> = LazyLock::new(|| {
+    [
+        TextureEntry::from_bytes(include_bytes!("../assets/red_circle.png")).unwrap(),
+        TextureEntry::from_bytes(include_bytes!("../assets/purple_circle.png")).unwrap(),
+        TextureEntry::from_bytes(include_bytes!("../assets/electric_circle.png")).unwrap(),
+        TextureEntry::from_bytes(include_bytes!("../assets/red_square.png")).unwrap(),
+        TextureEntry::from_bytes(include_bytes!("../assets/purple_square.png")).unwrap(),
+    ]
+});
